fff92a091a61b28ec71412e68af2303480558b96
MysteryCoder <srikar0811@gmail.com>
2025-07-11 17:36:13 +0530

    game of life

---
+++ game_of_life.py
@@ -0,0 +1,7 @@
+"""
+John Conway's Game of Life is a famous of simple rules giving
+rise to complex and a times chaotic structures.
+The problem of deciding weather the game continues or not is
+an NP problem
+
+"""
