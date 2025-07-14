3fb7da1a8222b6c5264bdb9ceeff7b87eb7c1703
MysteryCoder <srikar0811@gmail.com>
2025-07-14 15:02:52 +0530

    mandelbrot func

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,11 +3,21 @@ from manim import *
 
 """
 The Mandelbrot Set is the set of all Complex Numbers, c
-such that the function f(Z) = Z^2 + c, starting with Z = 0, stays bounded, it should not
-diverge to infinity.
-c = 1,
+such that the sequence Zn+1 = Zn^2 + c, starting with a  Z0 = 0, stays bounded(it should not
+diverge to infinity)
 
 """
+
+
+def testMandelbrot(c, max_iterations=100):
+    Z = 0
+    for _ in range(max_iterations):
+        Z = Z**2 + c
+
+        if abs(Z) > 2:
+            return False
+    return True
+
 
 class Mandelbrot(Scene):
     def construct(self):
